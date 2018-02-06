c3bba9dbb6cbf93f30da9408741780a6ae2413d5
Ari Kramer <arikramer24@gmail.com>
2018-02-06 09:13:25 -0500

    eh

--- utils/seed.js
+++ utils/seed.js
@@ -25,7 +25,7 @@ const seed = async () => {
 
   let data = combineMetrics(bpi, kpi.data, nolan.data);
 
-  FullData.findOrCreate({
+  return FullData.findOrCreate({
     where: { id: 1 }
   })
     .spread((fullData, isCreated) => {
