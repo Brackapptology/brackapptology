fd85ebd92ccbfaff456deb0b5383dbe1ce9d7dd7
Ari Kramer <arikramer24@gmail.com>
2018-02-06 09:16:14 -0500

    invoked seed

--- utils/seed.js
+++ utils/seed.js
@@ -25,7 +25,7 @@ const seed = async () => {
 
   let data = combineMetrics(bpi, kpi.data, nolan.data);
 
-  return FullData.findOrCreate({
+  FullData.findOrCreate({
     where: { id: 1 }
   })
     .spread((fullData, isCreated) => {
@@ -36,5 +36,7 @@ const seed = async () => {
     })
     .catch(console.error)
 }
+
+seed();
 
 module.exports = seed;
